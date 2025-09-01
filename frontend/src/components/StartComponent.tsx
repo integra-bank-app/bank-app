ed88fc96388fe0f777572b024a0715df37892715
Andrei Atodiresei <atodireseiandrei833@yahoo.com>
2025-09-01 17:40:35 +0300

    Initial commit

--- frontend/src/components/StartComponent.tsx
+++ frontend/src/components/StartComponent.tsx
@@ -1,11 +1,10 @@
-import {JSX} from "react";
-import {Button} from "primereact/button";
+import { Button } from "primereact/button";
 
-export function StartComponent(): JSX.Element {
-    return (
-        <div>
-            <div className="text-5xl font-italic">test</div>
-            <Button icon="pi pi-search">Fancy Button</Button>
-        </div>
-    )
+export function StartComponent(): React.ReactNode {
+	return (
+		<div>
+			<div className="text-5xl font-italic">test</div>
+			<Button icon="pi pi-search">Fancy Button</Button>
+		</div>
+	);
 }
