9d815809c6154d9eae3d4efefd49286bd3ca3113
MadalinaMera <166218542+MadalinaMera@users.noreply.github.com>
2025-09-03 16:48:50 +0300

    feat: get users by branch

--- backend/src/main/java/clf/integra/backend/controller/TestController.java
+++ backend/src/main/java/clf/integra/backend/controller/TestController.java
@@ -3,12 +3,16 @@ package clf.integra.backend.controller;
 
 import clf.integra.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import clf.integra.backend.repository.UserRepository;
 
 
+import java.util.List;
+import java.util.UUID;
 @RestController
 public class TestController {
     private final UserRepository userRepository;
@@ -29,5 +33,23 @@ public class TestController {
     public String testRepo() {
         userService.addUserByName("A", "B", "C");
         return userRepository.getAllUsers().toString();
+    }
+
+    // Endpoint to get users by branchId
+    @GetMapping("branches/{branchId}/users")
+    public ResponseEntity<List<String>> getUsersByBranch(@PathVariable UUID branchId) {
+        List<String> users = userService.getAllUsersByBranch(branchId);
+        if (users.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        return ResponseEntity.ok(users);
+    }
+
+    // Test endpoint to get users by branchId
+    @GetMapping("/test/users")
+    public ResponseEntity<List<String>> testFirstBranchUsers() {
+        UUID existingBranchId = userRepository.getAllUsers().getFirst().getBranchId();
+        return getUsersByBranch(existingBranchId);
     }
 }
