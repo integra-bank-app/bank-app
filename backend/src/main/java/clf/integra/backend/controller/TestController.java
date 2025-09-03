466ad40585a1bd46d4582f87c524bb043dab45f0
marcus <marcus.coman@stud.ubbcluj.ro>
2025-09-03 14:55:06 +0300

    feat: add new user with Full Name, return user UUID.

--- backend/src/main/java/clf/integra/backend/controller/TestController.java
+++ backend/src/main/java/clf/integra/backend/controller/TestController.java
@@ -1,21 +1,22 @@
 package clf.integra.backend.controller;
 
 
+import clf.integra.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import clf.integra.backend.model.User;
 import clf.integra.backend.repository.UserRepository;
-
+import clf.integra.backend.service.UserService;
 
 @RestController
 public class TestController {
-
-
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    public TestController(UserRepository userRepository) {
+    public TestController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     @GetMapping("/test")
@@ -26,6 +27,7 @@ public class TestController {
     //Testing creation of the repository
     @GetMapping("/test-repo")
     public String testRepo() {
+        userService.addUserByName("A", "B", "C");
         return userRepository.getAllUsers().toString();
     }
 }
--- backend/src/main/java/clf/integra/backend/service/UserService.java
+++ backend/src/main/java/clf/integra/backend/service/UserService.java
@@ -1,6 +1,10 @@
 package clf.integra.backend.service;
+
+import clf.integra.backend.model.User;
 import clf.integra.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -9,4 +13,18 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    /* Input: Full name of the User. Output: UUID of the newly added User*/
+    public UUID addUserByName(String firstName, String middleName, String lastName) {
+        UUID uuid = generateUUID();
+        User newUser = new User(uuid, firstName, middleName, lastName, 0);
+        userRepository.addUser(newUser);
+        return uuid;
+    }
+
+    /* Added a generateUUID in case we want to change how the UUID is generated */
+    public UUID generateUUID() {
+        return UUID.randomUUID();
+    }
 }
+
