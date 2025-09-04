f671b2df0184f3fdf5b37ccfbabce8ed7fc1fbc2
lucianpopescu688 <lucianpopescu688@gmail.com>
2025-09-04 13:15:17 +0300

    feat: implemented the taxes and fees collection endpoint

--- backend/src/main/java/clf/integra/backend/controller/TestController.java
+++ backend/src/main/java/clf/integra/backend/controller/TestController.java
@@ -2,12 +2,16 @@ package clf.integra.backend.controller;
 
 
 import clf.integra.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import clf.integra.backend.repository.UserRepository;
 
 import java.util.UUID;
+
 
 @RestController
 public class TestController {
@@ -28,5 +32,21 @@ public class TestController {
     @GetMapping("/test-repo")
     public String testRepo() {
         return userRepository.getAllUsers().toString();
+    }
+
+    //Testing the taxes and fees system
+    @PostMapping("branches/{branchId}/collect-taxes-and-fees")
+    public ResponseEntity<Double> collectTaxesAndFeesFromBranch(@PathVariable UUID branchId) {
+        try {
+            double revenue = userService.collectTaxesAndFeesFromBranch(branchId);
+            if (revenue == 0) {
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null);
+            }
+
+            return ResponseEntity.ok(revenue);
+
+        } catch(IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 }
--- backend/src/main/java/clf/integra/backend/service/UserService.java
+++ backend/src/main/java/clf/integra/backend/service/UserService.java
@@ -5,6 +5,8 @@ import clf.integra.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,6 +19,34 @@ public class UserService {
         User newUser = new User(uuid, firstName, middleName, lastName, 0);
         userRepository.addUser(newUser);
         return uuid;
+    }
+
+    public double collectTaxesAndFeesFromBranch(UUID branchId) {
+        // We will be able to use the method of get clients for a branch implemented by Madalina
+        // after merging the develop branch with the branch she created.
+        List<User> allUsers = userRepository.getAllUsers();
+        List<User> usersBranch = new ArrayList<>();
+        double revenue = 0;
+
+        for (User user: allUsers) {
+            if (branchId.equals(user.getId())) {
+                usersBranch.add(user);
+            }
+        }
+
+        if (usersBranch.isEmpty()) {
+            throw  new IllegalArgumentException("The branch doesnt not have any customer!");
+        }
+
+        for (User user : usersBranch) {
+            double fee = user.getBalance() < 100 ? user.getBalance() * 0.1 : 10;
+            if (fee > 0) {
+                user.setBalance(user.getBalance() - fee);
+                revenue += fee;
+            }
+        }
+
+        return revenue;
     }
 
     public UUID generateUUID() {
