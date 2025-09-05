5ee8ef04b46bbd2b637fe69df6d084b35f047165
Andrei Atodiresei <atodireseiandrei833@yahool.com>
2025-09-05 18:48:19 +0300

    feat(api): add topup endpoint for increasing users balance

--- backend/src/main/java/clf/integra/backend/controller/UserController.java
+++ backend/src/main/java/clf/integra/backend/controller/UserController.java
@@ -1,8 +1,12 @@
 package clf.integra.backend.controller;
 
+import clf.integra.backend.dto.BalanceDTO;
 import clf.integra.backend.dto.UserDTO;
 import clf.integra.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +21,23 @@ public class UserController {
     @PostMapping("/users")
     public UUID addUser(@RequestBody UserDTO user) {
         return userService.addUserWithName(user.firstName(), user.middleName(), user.lastName());
+    }
+
+    @PostMapping("/users/{userId}/balance")
+    public ResponseEntity<Double> addUserBalance(@PathVariable("userId") UUID userId, @RequestBody BalanceDTO balance) {
+        double value = balance.value();
+        if (value < 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        //Simulate a random chance of 20% (in the issue is 10%, but I had bad luck) for the operation to fail
+        double randomValue = Math.random();
+        if (randomValue >= 0.8) {
+            return new ResponseEntity<>(HttpStatus.PAYMENT_REQUIRED);
+        }
+
+        double finalBalance = userService.addBalance(userId, value);
+        return new ResponseEntity<>(finalBalance, HttpStatus.OK);
+
     }
 }
--- backend/src/main/java/clf/integra/backend/service/UserService.java
+++ backend/src/main/java/clf/integra/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package clf.integra.backend.service;
 
+import clf.integra.backend.exceptions.NotFoundException;
 import clf.integra.backend.model.User;
 import clf.integra.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,17 @@ public class UserService {
         User newUser = new User(uuid, firstName, middleName, lastName, 0);
         userRepository.addUser(newUser);
         return uuid;
+    }
+
+    public double addBalance(UUID uuid, double amount) {
+        if (!userRepository.userExists(uuid)) {
+            throw new NotFoundException("User not found");
+        }
+
+        User user = userRepository.getUserById(uuid);
+        user.setBalance(user.getBalance() + amount);
+
+        return user.getBalance();
     }
 
     public UUID generateUUID() {
