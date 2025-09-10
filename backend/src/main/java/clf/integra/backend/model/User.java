9304217cf3a20316a18e1f0bf34e580355f66f0d
Andrei Atodiresei <atodireseiandrei833@yahoo.com>
2025-09-10 10:25:48 +0300

    feat(#31): add jpa annotations to user entity

--- backend/src/main/java/clf/integra/backend/model/User.java
+++ backend/src/main/java/clf/integra/backend/model/User.java
@@ -1,33 +1,49 @@
 package clf.integra.backend.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NonNull;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
-public class User {
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
 
-    @NonNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @Setter
+    @Column(nullable = false)
     private String firstName;
 
     @Setter
     private String middleName;
 
     @Setter
+    @Column(nullable = false)
     private String lastName;
 
     @Setter
+    @Column(nullable = false)
     private double balance;
 
     @Setter
+    @Column(nullable = false)
     private UUID branchId;
 }
