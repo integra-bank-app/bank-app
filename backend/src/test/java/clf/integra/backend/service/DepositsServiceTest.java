d6d1c34ea1aaf9de1de7a944fdb32fbdf26645eb
Andrei Atodiresei <atodireseiandrei833@yahoo.com>
2025-09-16 15:06:03 +0300

    feat(#61): add deposits service tests and add builder defaults to some entities

--- backend/src/test/java/clf/integra/backend/service/DepositsServiceTest.java
+++ backend/src/test/java/clf/integra/backend/service/DepositsServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 import java.util.UUID;
