f41ad4812f8b427595e591b1447b9e753f56ad52
Andrei Atodiresei <atodireseiandrei833@yahool.com>
2025-09-17 20:10:45 +0300

    feat(#60): add notification entity and dependency for sockets

---
+++ backend/src/main/java/clf/integra/backend/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package clf.integra.backend.repository;
+
+import clf.integra.backend.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+}
