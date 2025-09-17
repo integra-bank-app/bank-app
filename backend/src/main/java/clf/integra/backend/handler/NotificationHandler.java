5242ae0721ddbb54dece4515af537d600c6ad33e
Andrei Atodiresei <atodireseiandrei833@yahool.com>
2025-09-17 20:11:26 +0300

    feat(#60): add socket handling

---
+++ backend/src/main/java/clf/integra/backend/handler/NotificationHandler.java
@@ -0,0 +1,73 @@
+package clf.integra.backend.handler;
+
+import clf.integra.backend.mapper.NotificationMapper;
+import clf.integra.backend.model.Notification;
+import clf.integra.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationHandler extends TextWebSocketHandler {
+
+    private final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    private final UserRepository userRepository;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+        String uuid = getUUIDFromQuery(session);
+        if (uuid == null) {
+            session.close(CloseStatus.NOT_ACCEPTABLE);
+            return;
+        }
+
+        UUID userUuid;
+        try {
+            userUuid = UUID.fromString(uuid);
+        } catch (IllegalArgumentException e) {
+            session.close(CloseStatus.NOT_ACCEPTABLE);
+            return;
+        }
+
+        if (userRepository.findById(userUuid).isEmpty())
+        {
+            session.close(CloseStatus.BAD_DATA);
+            return;
+        }
+
+        sessions.put(uuid, session);
+        System.out.println("Connected to notifications " + uuid);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status){
+        sessions.values().remove(session);
+    }
+
+    private String getUUIDFromQuery(WebSocketSession session) {
+        String query = session.getUri().getQuery();
+        if (query != null && query.startsWith("uuid=")) {
+            return query.substring(5);
+        }
+        return null;
+    }
+
+    public void sendNotification(Notification notification) throws IOException {
+        WebSocketSession session = sessions.get(notification.getUser().getId().toString());
+        if (session != null && session.isOpen()) {
+            String json = NotificationMapper.toJson(notification);
+            session.sendMessage(new TextMessage(json));
+        }
+    }
+}
---
+++ backend/src/main/java/clf/integra/backend/service/NotificationService.java
@@ -0,0 +1,48 @@
+package clf.integra.backend.service;
+
+import clf.integra.backend.handler.NotificationHandler;
+import clf.integra.backend.model.Notification;
+import clf.integra.backend.model.NotificationType;
+import clf.integra.backend.model.User;
+import clf.integra.backend.repository.NotificationRepository;
+import clf.integra.backend.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+
+    private final NotificationHandler notificationHandler;
+
+    @Transactional
+    public void sendNotificationToUser(NotificationType notificationType, String message, UUID userId) {
+        User user = userRepository.findById(userId).orElseThrow(EntityNotFoundException::new);
+
+        Notification notification = Notification.builder()
+                .user(user)
+                .type(notificationType)
+                .message(message)
+                .build();
+        user.getNotifications().add(notification);
+        userRepository.save(user);
+        notificationRepository.save(notification);
+
+        try
+        {
+        notificationHandler.sendNotification(notification);
+        }
+        catch (Exception e)
+            {
+            e.printStackTrace();
+            }
+    }
+
+}
