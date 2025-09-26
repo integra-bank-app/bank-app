e147d5bf3dfb27ce85eb2974ec925bc5076ab68d
Andrei Atodiresei <atodireseiandrei833@yahoo.com>
2025-09-26 17:23:37 +0300

    feat(#73): Add react-icons dependency and implement pagination in UserListPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,34 +2,54 @@ import { PrimeReactProvider } from "primereact/api";
 import { StartComponent } from "./components/StartComponent";
 
 import "primeflex/primeflex.css"; // flex
-import "primeicons/primeicons.css"; //icons
+import "primeicons/primeicons.css"; // icons
 import "primereact/resources/themes/bootstrap4-dark-purple/theme.css";
 import "./App.css";
 import { NotificationProvider } from "./contexts/NotificationProvider";
 import AdminPageComponent from "./pages/AdminPage";
-import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import UserListPage from "./pages/UserListPage";
 import { UserProvider } from "./contexts/UserProvider";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
 
 function App() {
 	return (
-		<>
-			<PrimeReactProvider>
-				<div className="p-m-4 p-p-4 surface-card border-round">
-					<UserProvider>
-						<NotificationProvider>
-							<BrowserRouter>
+		<PrimeReactProvider>
+			<UserProvider>
+				<NotificationProvider>
+					<BrowserRouter>
+						{/* Use flexbox layout to make footer stick to the bottom */}
+						<div className="flex flex-col min-h-screen">
+							{/* Header */}
+							<Header />
+
+							{/* Main Content */}
+							<main className="flex-grow p-4">
 								<Routes>
+									<Route path="/login" element={<Login />} />
 									<Route path="/" element={<StartComponent />} />
 									<Route path="/admin" element={<AdminPageComponent />} />
 									<Route path="/users" element={<UserListPage />} />
+									<Route
+										path="*"
+										element={
+											<div className="flex align-items-center justify-content-center">
+												404 Not Found
+											</div>
+										}
+									/>
 								</Routes>
-							</BrowserRouter>
-						</NotificationProvider>
-					</UserProvider>
-				</div>
-			</PrimeReactProvider>
-		</>
+							</main>
+
+							{/* Footer */}
+							<Footer />
+						</div>
+					</BrowserRouter>
+				</NotificationProvider>
+			</UserProvider>
+		</PrimeReactProvider>
 	);
 }
 
