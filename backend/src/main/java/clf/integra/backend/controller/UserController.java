d0bd6c71c38852deb06a0b77a9d82a905494c6ca
MadalinaMera <166218542+MadalinaMera@users.noreply.github.com>
2025-09-26 12:16:55 +0300

    feature(#74): removed CrossOrigin annotation from UserController

--- backend/src/main/java/clf/integra/backend/controller/UserController.java
+++ backend/src/main/java/clf/integra/backend/controller/UserController.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("*")
