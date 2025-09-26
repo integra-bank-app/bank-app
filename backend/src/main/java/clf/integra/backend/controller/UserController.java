0abcb6c1d1f16e9086a85374e296510d41f7f3ab
MadalinaMera <166218542+MadalinaMera@users.noreply.github.com>
2025-09-26 10:35:30 +0300

    feature(#74): updated notification system

--- backend/src/main/java/clf/integra/backend/controller/UserController.java
+++ backend/src/main/java/clf/integra/backend/controller/UserController.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
