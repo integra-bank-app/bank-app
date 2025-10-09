f228a8127fdb837327f4db9107610260cf394eb8
MadalinaMera <166218542+MadalinaMera@users.noreply.github.com>
2025-10-09 14:41:07 +0300

    feature(#112): anaf service configuration + method that returns random user salary

---
+++ ANAF/src/test/java/clf/integra/anaf/AnafApplicationTests.java
@@ -0,0 +1,13 @@
+package clf.integra.anaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
