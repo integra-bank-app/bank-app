9ce90b583deaff3d5464d744ce5d6f57a502ec98
MadalinaMera <166218542+MadalinaMera@users.noreply.github.com>
2025-10-09 17:37:01 +0300

    feature(#112): removed testing method

--- ANAF/src/main/java/clf/integra/anaf/AnafApplication.java
+++ ANAF/src/main/java/clf/integra/anaf/AnafApplication.java
@@ -1,28 +1,12 @@
 package clf.integra.anaf;
 
-import clf.integra.anaf.service.AnafService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.UUID;
 
 @SpringBootApplication
 public class AnafApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnafApplication.class, args);
-    }
-
-    /*
-        * Just for manual testing purposes
-     */
-    @Bean
-    CommandLineRunner demo(AnafService anafService) {
-        return args -> {
-            UUID userId = UUID.randomUUID();
-            anafService.getUserSalary(userId);
-        };
     }
 }
