1101af0ef27acc3faa3f3fe718227d8f26e0c95b
MadalinaMera <166218542+MadalinaMera@users.noreply.github.com>
2025-10-20 16:43:10 +0300

    feature(#119): send message request into queue logic implementation

---
+++ backend/src/main/java/clf/integra/backend/producer/ProducerConfig.java
@@ -0,0 +1,28 @@
+package clf.integra.backend.producer;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProducerConfig {
+    public static final String ANAF_TO_INTEGRA = "anaf-to-integra";
+    public static final String INTEGRA_TO_ANAF = "integra-to-anaf";
+
+    @Bean
+    public Queue anafToIntegra() {
+        return new Queue(ANAF_TO_INTEGRA, true);
+    }
+
+    @Bean
+    public Queue integraToAnaf() {
+        return new Queue(INTEGRA_TO_ANAF, true);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
