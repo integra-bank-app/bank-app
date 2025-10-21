8cc08765f67f175dc0a97f0a4594280cf4e4878a
zepciucdenisa <zepciuc.denisa@stud.ubbcluj.ro>
2025-10-21 14:12:54 +0300

    feature(#120) : modify the existing method in ANAF

--- ANAF/src/main/java/clf/integra/anaf/service/AnafService.java
+++ ANAF/src/main/java/clf/integra/anaf/service/AnafService.java
@@ -11,9 +11,21 @@ public class AnafService {
 
     private static final Logger logger = LoggerFactory.getLogger(AnafService.class);
 
+    private final RabbitTemplate rabbitTemplate;
+    private final String queueName;
+
+    public AnafService(RabbitTemplate rabbitTemplate, @Value("${queue.integraPay.name:integraPayQueue}") String queueName) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.queueName = queueName;
+    }
+
     public int getUserSalary(UUID id) {
         int salary = (int) (Math.random() * (250000 - 2500 + 1) + 2500);
         logger.info("User with id {} has {} salary", id, salary);
+
+        String meesage = String.format("{\"userId\": \"%s\", \"salary\": %d}", id, salary);
+        rabbitTemplate.convertAndSend(queueName, message);
+        logger.info("Sent salary message for user {} to queue {}", id, queueName);
         return salary;
     }
 }
